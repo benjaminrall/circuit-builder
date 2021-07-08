454e978fa5e8e01f36cabfba8fe77fc83dc1378d
benjaminrall <benr2508@aol.com>
2021-07-09 00:47:37 +0100

    Create main.py

---
+++ build0/main.py
@@ -0,0 +1,37 @@
+import pygame
+import os
+from personallib.camera import Camera
+
+# Constants
+WIN_WIDTH = 800
+WIN_HEIGHT = 800
+FRAMERATE = 120
+ICON_IMG = pygame.image.load(os.path.join("imgs", "icon.png"))
+
+# Pygame Setup
+win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+pygame.display.set_caption("NAME")
+pygame.display.set_icon(ICON_IMG)
+clock = pygame.time.Clock()
+
+# Objects
+cam = Camera(win, 0, 0, 1)
+
+# Variables
+running = True
+
+# Main Loop
+if __name__ == '__main__':
+    while running:
+
+        dt = clock.tick(FRAMERATE) * 0.001
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                exit()
+                    
+        win.fill((255, 255, 255))
+        
+        pygame.display.update()
